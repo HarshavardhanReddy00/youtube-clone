e6598bd56fc6334f62a055ec302ecb2436ce09dd
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:46:21 +0530

    added dummy movies page

---
+++ youtube-clone/frontend/src/pages/Movies.jsx
@@ -0,0 +1,9 @@
+const Movies = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Movies Page
+    </div>
+  );
+};
+
+export default Movies;
