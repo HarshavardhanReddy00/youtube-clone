ba97a78396251d8b43115e0817bad55a8e2f0d4e
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:51:21 +0530

    created dummy reporthistory page

---
+++ youtube-clone/frontend/src/pages/ReportHistory.jsx
@@ -0,0 +1,9 @@
+const ReportHistory = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Report History Page
+    </div>
+  );
+};
+
+export default ReportHistory;
