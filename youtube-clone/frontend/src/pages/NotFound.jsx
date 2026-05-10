dfad03c84a875f8c6eca8fff5680efe924f80969
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:47:16 +0530

    added not found page

---
+++ youtube-clone/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="bg-black text-white h-screen flex justify-center items-center text-4xl">
+      404 Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
