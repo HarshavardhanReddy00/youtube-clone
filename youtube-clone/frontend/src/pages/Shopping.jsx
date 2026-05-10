84b7ded6958e9f020e15ca9b7970a77f8d0bf067
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:52:25 +0530

    created dummy shopping page

---
+++ youtube-clone/frontend/src/pages/Shopping.jsx
@@ -0,0 +1,9 @@
+const Shopping = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Shopping Page
+    </div>
+  );
+};
+
+export default Shopping;
