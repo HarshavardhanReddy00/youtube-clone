495e1e105b1cb220156a0b19430294966eb7177d
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:47:55 +0530

    added dummy playlist page

---
+++ youtube-clone/frontend/src/pages/Playlists.jsx
@@ -0,0 +1,9 @@
+const Playlists = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Playlists Page
+    </div>
+  );
+};
+
+export default Playlists;
