66a29c331b9ec86ef8b08f9f8c33dfb98a227042
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:42:36 +0530

    added history dummy page

---
+++ youtube-clone/frontend/src/pages/History.jsx
@@ -0,0 +1,9 @@
+const History = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Watch History
+    </div>
+  );
+};
+
+export default History;
