c517d2af957353cd583beea2ff7cead79457b2b9
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:53:22 +0530

    created dummy subscriptions page

---
+++ youtube-clone/frontend/src/pages/Subscriptions.jsx
@@ -0,0 +1,9 @@
+const Subscriptions = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Subscriptions Page
+    </div>
+  );
+};
+
+export default Subscriptions;
