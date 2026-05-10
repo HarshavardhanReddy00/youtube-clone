655df2da3b76c75eb6eabd002b53e8a8ebd48429
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 19:26:32 +0530

    added controller for login

--- youtube-clone/backend/src/controllers/authController.js
+++ youtube-clone/backend/src/controllers/authController.js
@@ -45,3 +45,36 @@ export const registerUser = async (req, res) => {
   }
 };
 
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid Credentials"
+      });
+    }
+
+    const match = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!match) {
+      return res.status(400).json({
+        message: "Invalid Credentials"
+      });
+    }
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
