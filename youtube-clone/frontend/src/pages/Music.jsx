43a378ac3ed18330ad3d05578ad5cf69948ae7db
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:46:46 +0530

    added dummy music page

---
+++ youtube-clone/frontend/src/pages/Music.jsx
@@ -0,0 +1,9 @@
+const Music = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Music Page
+    </div>
+  );
+};
+
+export default Music;
