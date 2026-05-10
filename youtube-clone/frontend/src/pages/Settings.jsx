eea4ea6772e93499f16a208f4b3d80d74abfab73
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:51:58 +0530

    created dummy settings page

---
+++ youtube-clone/frontend/src/pages/Settings.jsx
@@ -0,0 +1,9 @@
+const Settings = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Settings Page
+    </div>
+  );
+};
+
+export default Settings;
