319407148b0ae219dd76a00d8c4da53bdec5ba5e
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 19:26:06 +0530

    added controller for register

---
+++ youtube-clone/backend/src/controllers/authController.js
@@ -0,0 +1,47 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters"
+      });
+    }
+
+    const emailRegex =
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Invalid Email"
+      });
+    }
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: "User already exists"
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      username,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "Registered Successfully"
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
