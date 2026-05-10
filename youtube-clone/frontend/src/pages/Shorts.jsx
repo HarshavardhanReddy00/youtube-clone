4f221badd031acd945ca0b75b896495d2ce1354f
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 18:52:48 +0530

    created dummy shorts page

---
+++ youtube-clone/frontend/src/pages/Shorts.jsx
@@ -0,0 +1,9 @@
+const Shorts = () => {
+  return (
+    <div className="bg-black text-white min-h-screen flex justify-center items-center text-4xl font-bold">
+      Shorts Page
+    </div>
+  );
+};
+
+export default Shorts;
