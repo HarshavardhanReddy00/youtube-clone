7bd99df2a964fc5e47ead63c56bf00890031c4af
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-05-10 19:29:36 +0530

    added controller for update and delete channel

--- youtube-clone/backend/src/controllers/channelController.js
+++ youtube-clone/backend/src/controllers/channelController.js
@@ -86,3 +86,94 @@ export const getChannel = async (req, res) => {
   }
 };
 
+// Update Channel
+export const updateChannel = async (
+  req,
+  res
+) => {
+  try {
+    const channel = await Channel.findById(
+      req.params.id
+    );
+
+    if (!channel) {
+      return res.status(404).json({
+        message: "Channel not found"
+      });
+    }
+
+    if (
+      channel.owner.toString() !==
+      req.user._id.toString()
+    ) {
+      return res.status(401).json({
+        message: "Not Authorized"
+      });
+    }
+
+    channel.channelName =
+      req.body.channelName ||
+      channel.channelName;
+
+    channel.description =
+      req.body.description ||
+      channel.description;
+
+    channel.banner =
+      req.body.banner ||
+      channel.banner;
+
+    const updatedChannel =
+      await channel.save();
+
+    res.json(updatedChannel);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+// Delete Channel
+export const deleteChannel = async (
+  req,
+  res
+) => {
+  try {
+    const channel = await Channel.findById(
+      req.params.id
+    );
+
+    if (!channel) {
+      return res.status(404).json({
+        message: "Channel not found"
+      });
+    }
+
+    if (
+      channel.owner.toString() !==
+      req.user._id.toString()
+    ) {
+      return res.status(401).json({
+        message: "Not Authorized"
+      });
+    }
+
+    // Delete All Videos Of Channel
+    await Video.deleteMany({
+      channel: channel._id
+    });
+
+    await channel.deleteOne();
+
+    res.json({
+      message:
+        "Channel Deleted Successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
